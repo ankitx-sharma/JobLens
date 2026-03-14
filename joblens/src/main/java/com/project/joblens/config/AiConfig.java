391ea31998b6e428208af4b1cadee7eb3b7463a5
Ankita Sharma <ankitasharma.281993@gmail.com>
2026-03-15 00:16:56 +0100

    changes in the config file

--- joblens/src/main/java/com/project/joblens/config/AiConfig.java
+++ joblens/src/main/java/com/project/joblens/config/AiConfig.java
@@ -1,7 +1,10 @@
 package com.project.joblens.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.mistralai.MistralAiChatModel;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +12,20 @@ import org.springframework.context.annotation.Configuration;
 public class AiConfig {
 	
 	@Bean
-	public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
+	@Qualifier("llamaChatClient")
+	public ChatClient llamaChatClient(OllamaChatModel ollamaChatModel) {
 		return ChatClient.builder(ollamaChatModel).build();
+	}
+	
+	@Bean
+	@Qualifier("mistralChatClient")
+	public ChatClient mistralChatClient(MistralAiChatModel mistralaiChatModel) {
+		return ChatClient.builder(mistralaiChatModel).build();
+	}
+	
+	@Bean
+	@Qualifier("gptChatClient")
+	public ChatClient gptChatClient(OpenAiChatModel openAiChatModel) {
+		return ChatClient.builder(openAiChatModel).build();
 	}
 }
